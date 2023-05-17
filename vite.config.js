d7582cac2bc5962885bfc977206eb38de5182f85
sd4483 <sudheer.paturi@hotmail.com>
2023-05-17 04:51:27 +0100

    made some changes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        basicSsl(),
     ],
 });
