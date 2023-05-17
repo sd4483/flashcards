3978bff02d1b517c3f4062056d766c8c822bcfec
sd4483 <sudheer.paturi@hotmail.com>
2023-05-17 04:39:43 +0100

    reverting AppServiceProvider changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
