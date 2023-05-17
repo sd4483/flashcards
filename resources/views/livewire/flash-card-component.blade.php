466ea2e4092f3d3276792ad3777a7ea04e611e90
sd4483 <sudheer.paturi@hotmail.com>
2023-05-17 05:13:41 +0100

    made the fields wider

--- resources/views/livewire/flash-card-component.blade.php
+++ resources/views/livewire/flash-card-component.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-center">
-    <div class="w-full max-w-lg mt-8 ml-2 mr-2">
+    <div class="w-full max-w-xl mt-8 ml-2 mr-2">
         <h1 class="text-2xl mb-8 font-bold text-center text-white">FlashCards</h1>
         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" wire:submit.prevent="save">
             <input type="text" wire:model="form.question" placeholder="Title" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4">
